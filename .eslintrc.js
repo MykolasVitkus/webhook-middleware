931080642f3be40c3d4eda2c180836ad6772c730
Mykolas <mykolas.vitkus@plumtreesystems.com>
2021-04-06 14:30:23 +0000

    eslint lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,30 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
     extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:prettier/recommended",
-      "prettier/@typescript-eslint",
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
     ],
     env: {
-      browser: true,
-      es6: true,
-      node: true,
+        browser: true,
+        es6: true,
+        node: true,
     },
     overrides: [
-      {
-        files: ["*.tsx"],
-        rules: {
-          "react/prop-types": "off",
+        {
+            files: ['*.tsx'],
+            rules: {
+                'react/prop-types': 'off',
+            },
         },
-      },
-      {
-        files: ["*.js"],
-        rules: {
-          "@typescript-eslint/no-var-requires": "off",
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off',
+            },
         },
-      },
     ],
-  };
-  
+};
