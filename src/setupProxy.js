5563a653a8c32ac238e3a398950835bb61cf0ee6
Mykolas <mykolas.vitkus@plumtreesystems.com>
2021-05-03 17:22:48 +0000

    dockerfile

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+require('dotenv-flow').config();
 
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://bakis_app_1:3001',
+            target: process.env.SERVER_URL,
             changeOrigin: true,
             pathRewrite: {
                 '/api': '',
